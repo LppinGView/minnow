c19499ecf02b5de9826274b5ac21a3d35da1a86e
LppinGView <278019309@qq.com>
2024-05-29 10:07:25 +0800

    字节流lab0作业

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,60 +6,66 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return streamClose;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if (data =="" || available_capacity()<=0)
+  {
+    return;
+  }
+  auto dataLen = min(data.length(), available_capacity());
+  buffer+=data.substr(0, dataLen);
+  wIndex+=dataLen;
+  pushedNum+=dataLen;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  streamClose=true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - wIndex;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return pushedNum;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return writer().is_closed() && wIndex==0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return poppedNum;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if (wIndex > 0 && !buffer.empty()) {
+    return std::string_view(buffer.data(), buffer.size());
+  }
+  return "";
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  if (len>0 && wIndex>0)
+  {
+    auto eraseLen = min(wIndex, len);
+    buffer=buffer.substr(eraseLen, wIndex-eraseLen);
+    wIndex-=eraseLen;
+    poppedNum+=eraseLen;
+  }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return wIndex;
 }
